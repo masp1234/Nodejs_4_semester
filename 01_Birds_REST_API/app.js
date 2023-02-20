5b85201814788ff1da2d20b12b655641ae351cb0
masp9 <mart2c04@stud.kea.dk>
2023-02-20 22:36:03 +0100

    Bird API full CRUD

--- 01_Birds_REST_API/app.js
+++ 01_Birds_REST_API/app.js
@@ -1,25 +1,28 @@
 const express = require('express');
 
 const app = express();
-// app.use(express.json());
+
+app.use(express.json());
 
 const PORT = 8080;
 
 const birds = [
     {
         id: 1,
-        name: "Dove"
+        name: "Dove",
+        averageWeightInGrams: 120
     },
     {
         id: 2,
-        name: "Hummingbird"
+        name: "Hummingbird",
+        averageWeightInGrams: 4
     },
     {
         id: 3,
-        name: "Seagull"
+        name: "Seagull",
+        averageWeightInGrams: 1900
     }
-]
-
+];
 
 app.get("/api/v1/birds", (req, res) => {
     res.status(200).send({ data: birds });
@@ -29,10 +32,43 @@ app.get("/api/v1/birds/:id", (req, res) => {
     // brug find i stedet for filter, da filter returnerer et array
     // brug for at lave reg.params.id om fra string til number
     const bird = birds.find(bird => bird.id === Number(req.params.id));
-    console.log(bird)
     res.status(200).send({ data: bird });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+app.post("/api/v1/birds", (req, res) => {
+    const bird = { id: generateId(), ...req.body };
+    birds.push(bird);
+    res.status(201).send({ data: bird });
 });
+
+app.put("/api/v1/birds/:id", (req, res) => {
+    const birdIndex = birds.findIndex(bird => bird.id === Number(req.params.id));
+    const updatedBird = { id: birds[birdIndex].id, ...req.body};
+
+    birds.splice(birdIndex, 1, updatedBird);
+    res.status(200).send({ data: updatedBird });
+});
+
+app.patch("/api/v1/birds/:id", (req, res) => {
+    const bird = birds.find(bird => bird.id === Number(req.params.id));
+    if (req.body.name != null) {
+        bird.name = req.body.name;
+    }
+    if (req.body.averageWeightInGrams != null) {
+        bird.averageWeightInGrams = req.body.averageWeightInGrams;
+    }
+    res.status(200).send({ data: bird });
+});
+
+app.delete("/api/v1/birds/:id", (req, res) => {
+    const birdIndex = birds.findIndex(bird => bird.id === Number(req.params.id));
+    // kan ikke bruge birds.filter, da den ikke ændrer på arrayet, men i stedet returnerer et nyt
+    birds.splice(birdIndex, 1)
+    res.status(200).send({ msg: `The bird with id: ${req.params.id} was removed successfully`});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+const generateId = () => birds.length + 1;
