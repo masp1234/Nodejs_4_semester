90181f3a1f4a09f34e5c3e68f012783564fac341
masp1234 <mart2c04@stud.kea.dk>
2023-02-21 12:40:57 +0100

    generateId() fixed

--- 01_Birds_REST_API/app.js
+++ 01_Birds_REST_API/app.js
@@ -24,6 +24,8 @@ const birds = [
     }
 ];
 
+let birdsLength = birds.length;
+
 app.get("/api/v1/birds", (req, res) => {
     res.status(200).send({ data: birds });
 });
@@ -32,6 +34,9 @@ app.get("/api/v1/birds/:id", (req, res) => {
     // brug find i stedet for filter, da filter returnerer et array
     // brug for at lave reg.params.id om fra string til number
     const bird = birds.find(bird => bird.id === Number(req.params.id));
+    if (!bird) {
+        return res.status(404).send({ msg: `The bird with id: ${req.params.id} was not found`});
+    };
     res.status(200).send({ data: bird });
 });
 
@@ -43,27 +48,36 @@ app.post("/api/v1/birds", (req, res) => {
 
 app.put("/api/v1/birds/:id", (req, res) => {
     const birdIndex = birds.findIndex(bird => bird.id === Number(req.params.id));
+    if (birdIndex < 0) {
+        return res.status(404).send({ msg: `The bird with id: ${req.params.id} was not found`});
+    };
+    
     const updatedBird = { id: birds[birdIndex].id, ...req.body};
-
     birds.splice(birdIndex, 1, updatedBird);
-    res.status(200).send({ data: updatedBird });
+    res.status(201).send({ data: updatedBird });
 });
 
 app.patch("/api/v1/birds/:id", (req, res) => {
     const bird = birds.find(bird => bird.id === Number(req.params.id));
+    if (!bird) {
+        return res.status(404).send({ msg: `The bird with id: ${req.params.id} was not found`});
+    };
     if (req.body.name != null) {
         bird.name = req.body.name;
-    }
+    };
     if (req.body.averageWeightInGrams != null) {
         bird.averageWeightInGrams = req.body.averageWeightInGrams;
-    }
+    };
     res.status(200).send({ data: bird });
 });
 
 app.delete("/api/v1/birds/:id", (req, res) => {
     const birdIndex = birds.findIndex(bird => bird.id === Number(req.params.id));
+    if (birdIndex < 0) {
+        return res.status(404).send({ msg: `The bird with id: ${req.params.id} was not found`});
+    };
     // kan ikke bruge birds.filter, da den ikke ændrer på arrayet, men i stedet returnerer et nyt
-    birds.splice(birdIndex, 1)
+    birds.splice(birdIndex, 1);
     res.status(200).send({ msg: `The bird with id: ${req.params.id} was removed successfully`});
 })
 
@@ -71,4 +85,4 @@ app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
-const generateId = () => birds.length + 1;
+const generateId = () => birdsLength += 1;
