ca0e9cd4d823b2127dc119870dd45491212c2833
masp1234 <mart2c04@stud.kea.dk>
2023-04-21 17:02:56 +0200

    deleted an unused file

--- 09__Solar_System/server/databases/createDatabase.js
+++ 09__Solar_System/server/databases/createDatabase.js
@@ -1,40 +1,42 @@
-import db from "./connection.js";
+import db from "./connection.js"
 
-const isDeleteMode = process.argv.findIndex((argument) => argument === "delete_mode") === -1 ? false : true;
+
+
+const isDeleteMode = process.argv.findIndex(argument => argument === "delete_mode") === -1 ? false : true
 
 if (isDeleteMode) {
-    db.exec(`DROP TABLE planets;`);
-    db.exec(`DROP TABLE people;`);
+    db.exec('DROP TABLE planets;')
+    db.exec('DROP TABLE people;')
 }
 
-// (DDL)
-db.exec(`
-CREATE TABLE IF NOT EXISTS planets (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    size FLOAT,
-    is_habitable BOOLEAN NOT NULL
-);
-`);
+
+// this is DDL - Data Definition Language
+// plural table names, like in REST endpoints, since it contains multiple entries - write in snake_case
+db.exec(
+    `CREATE TABLE IF NOT EXISTS planets(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        size FLOAT,
+        is_habitable BOOLEAN NOT NULL
+        );`
+)
 
 db.exec(`
 CREATE TABLE IF NOT EXISTS people (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    planet_id INTEGER,
-    FOREIGN KEY (planet_id) REFERENCES planets (id)
+    name TEXT
 );
-`);
-
-// Seeding (DML)
+`)
+// (Data Manipulation Language  DML
+// Seeding - the idea of putting data into your database
 if (isDeleteMode) {
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Mercury', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Venus', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Earth', True);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Mars', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Jupiter', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Saturn', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Uranus', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Neptune', False);`);
-}
+    db.exec(`INSERT into planets(name, is_habitable) VALUES ('Mercury', False)`)
+    db.exec(`INSERT into planets(name, is_habitable) VALUES ('Venus', False)`)
+    db.exec(`INSERT into planets(name, is_habitable) VALUES ('Earth', True)`)
+    db.exec(`INSERT into planets(name, is_habitable) VALUES ('Mars', False)`)
+    db.exec(`INSERT into planets(name, is_habitable) VALUES ('Jupiter', False)`)
+    db.exec(`INSERT into planets(name, is_habitable) VALUES ('Uranus', False)`)
+    db.exec(`INSERT into planets(name, is_habitable) VALUES ('Saturn', False)`)
+    db.exec(`INSERT into planets(name, is_habitable) VALUES ('Neptune', False)`)
 
+}
